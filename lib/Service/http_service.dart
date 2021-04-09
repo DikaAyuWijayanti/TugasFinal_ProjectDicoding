379172238652e05141c2a0df6f29712858a41325
= <=>
2021-04-09 15:37:35 +0700

    buat class untuk konek ke rest api themoviedb

---
+++ lib/Service/http_service.dart
@@ -0,0 +1,6 @@
+import 'package:http/http.dart' as http;
+
+class HttpService{
+  final String apiKey = 'isi dengan api key anda';
+  final String baseUrl = 'https://api.themoviedb.org/3/movie/popular?api_key=';
+}
