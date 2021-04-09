8fe66edc9148e3579db7e4c8043382a9b589f886
= <=>
2021-04-09 23:22:14 +0700

    buat function untuk mengkonversi json - response

--- lib/models/movie.dart
+++ lib/models/movie.dart
@@ -6,4 +6,12 @@ class Movie{
   String posterPath;
 
   Movie(this.id,this.title,this.voteAverage,this.overview, this.posterPath);
+
+  Movie.fromJson(Map<String, dynamic> parsedJson){
+    this.id = parsedJson['id'];
+    this.title = parsedJson['title'];
+    this.voteAverage = parsedJson['vote_average'] * 1.0;
+    this.overview = parsedJson['overview'];
+    this.posterPath = parsedJson['poster_path'];
+  }
 }
