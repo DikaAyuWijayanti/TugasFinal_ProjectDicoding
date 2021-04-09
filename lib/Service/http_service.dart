4adc1f8aff8f65a5b24ad871fcc8cfcc2a42f7d4
= <=>
2021-04-09 17:24:22 +0700

    membuat function untuk mengambil response server themoviedb.org

--- lib/Service/http_service.dart
+++ lib/Service/http_service.dart
@@ -1,6 +1,21 @@
+import 'dart:io';
 import 'package:http/http.dart' as http;
 
 class HttpService{
-  final String apiKey = 'isi dengan api key anda';
+  final String apiKey = '0ab77f6a9987c6bbb8d605078c4ddc79';
   final String baseUrl = 'https://api.themoviedb.org/3/movie/popular?api_key=';
+
+  Future<String> getPopularMovies() async{
+    final String uri = baseUrl + apiKey;
+
+    http.Response result = await http.get(Uri.parse(uri));
+    if(result.statusCode == HttpStatus.ok){
+      print("Sukses");
+      String response = result.body;
+      return response;
+    }else{
+      print("Fail");
+      return null;
+    }
+  }
 }
