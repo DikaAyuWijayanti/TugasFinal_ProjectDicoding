1bff3f9f81ab30920852de3c4460e805316cf086
= <=>
2021-04-10 05:40:24 +0700

    update function http_service.dart

--- lib/Service/http_service.dart
+++ lib/Service/http_service.dart
@@ -1,18 +1,22 @@
+import 'dart:convert';
 import 'dart:io';
 import 'package:http/http.dart' as http;
+import 'package:praktikum_7/models/movie.dart';
 
 class HttpService{
   final String apiKey = '0ab77f6a9987c6bbb8d605078c4ddc79';
   final String baseUrl = 'https://api.themoviedb.org/3/movie/popular?api_key=';
 
-  Future<String> getPopularMovies() async{
+  Future<List> getPopularMovies() async{
     final String uri = baseUrl + apiKey;
 
     http.Response result = await http.get(Uri.parse(uri));
     if(result.statusCode == HttpStatus.ok){
       print("Sukses");
-      String response = result.body;
-      return response;
+     final jsonResponse = json.decode(result.body);
+     final moviesMap = jsonResponse['results'];
+     List movies = moviesMap.map((i) => Movie.fromJson(i)).toList();
+     return movies;
     }else{
       print("Fail");
       return null;
