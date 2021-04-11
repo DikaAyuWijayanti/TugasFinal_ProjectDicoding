329b2b0d0bee0b13269645d8c940e4422fa71036
= <=>
2021-04-11 21:29:16 +0700

    menambah atribut

--- lib/models/movie.dart
+++ lib/models/movie.dart
@@ -1,17 +1,21 @@
+//class movie lengkap dengan variabel dan construktor
 class Movie{
   int id;
   String title;
   double voteAverage;
   String overview;
   String posterPath;
+  String releaseDate;
+  
 
-  Movie(this.id,this.title,this.voteAverage,this.overview, this.posterPath);
-
+  Movie(this.id,this.title,this.voteAverage,this.overview, this.posterPath,this.releaseDate);
+  //function untuk mengkonversi json menjadi response yang sesuai dengan class movie
   Movie.fromJson(Map<String, dynamic> parsedJson){
     this.id = parsedJson['id'];
     this.title = parsedJson['title'];
     this.voteAverage = parsedJson['vote_average'] * 1.0;
     this.overview = parsedJson['overview'];
     this.posterPath = parsedJson['poster_path'];
+    this.releaseDate = parsedJson['release_date'];
   }
 }
